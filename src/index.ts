b5fc2b3fd92cb8074e65b04ca4186d6701fbd244
Fabian Goetzens <fabian.goetzens@connectry.io>
2026-02-17 01:02:36 +0000

    fix: sync VERSION to 0.7.3

--- src/index.ts
+++ src/index.ts
@@ -19,7 +19,7 @@ import { registerPriceTools } from './tools/prices.js';
 import { registerOrderTools } from './tools/orders.js';
 import { registerArbitrageTools } from './tools/arbitrage.js';
 
-const VERSION = '0.7.0';
+const VERSION = '0.7.2';
 
 /**
  * Display startup banner
